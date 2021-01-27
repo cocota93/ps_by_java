849925ea7dda969ddc5326613770a503c000bc9e
jedy <cocota93@gmail.com>
2021-01-27 15:12:56 +0900

    30 : 보류

---
+++ src/com/ps/백준/그리디/_30/Main.java
@@ -0,0 +1,26 @@
+package com.ps.백준.그리디._30;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.StringTokenizer;
+
+/*
+* N이 최대 10^5개의 숫자라고 해서 순열조합으로는 안될것 같은데...어카라는거지?
+* https://pangsblog.tistory.com/87 풀이보면 이해는 가는데 다음에 또 이런문제가 나와도 풀수있을까?
+* */
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/com/ps/백준/그리디/_30/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+
+    }
+
+}

e0a0e717c44bfc22e0305276bd5855777b2cf656
jedy <cocota93@gmail.com>
2021-01-29 14:32:42 +0900

    하노이탑_이동순서 : 보류

---
+++ src/com/ps/백준/분할정복/풀지못한문제/하노이탑_이동순서/Main.java
@@ -0,0 +1,29 @@
+package com.ps.백준.분할정복.풀지못한문제.하노이탑_이동순서;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.util.StringTokenizer;
+
+/*
+* 뭔가 길찾기 비슷한 느낌이든다.
+* 이동횟수도 최소로 구해야되고 BFS로 풀면 되지않을까 싶다.
+* DFS도 잘어울려 보이는데??
+* 방문표시는 어떻게할까 생각해봤는데 3중배열로 해결하면 될듯? visited[5][5][5] 형태로?
+*
+* */
+
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/com/ps/백준/분할정복/하노이탑_이동순서/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+
+    }
+
+}

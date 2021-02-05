c2b2105d7514400c3005c415500b19e97717a804
jedy <cocota93@gmail.com>
2021-02-05 17:42:29 +0900

    _1로만들기 : 완료

--- src/com/ps/백준/DP/_1로만들기/Main.java
+++ src/com/ps/백준/DP/_1로만들기/Main.java
@@ -2,14 +2,14 @@ package com.ps.백준.DP._1로만들기;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
 
 
 class Main {
+    static int memo[];
 
     public static void main(String[] args) throws Exception {
 //        System.setIn(new FileInputStream("src/com/ps/백준/DP/_1로만들기/input.txt"));
@@ -20,23 +20,31 @@ class Main {
         int n = Integer.parseInt(br.readLine());
         int answer = 0;
 
-        while(n > 1){
-            answer++;
+        memo = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = 0;
+        for (int i = 2; i < memo.length; i++) {
+            memo[i] = memo[i - 1] + 1;
 
-            if(n % 3 == 0){
-                n = n / 3;
-                continue;
+//            if(i % 6 == 0){
+//                memo[i] = Math.min(memo[i], memo[i / 2] + 1);
+//                memo[i] = Math.min(memo[i], memo[i / 3] + 1);
+//            } else
+//
+            if(i % 2 == 0) {
+                memo[i] = Math.min(memo[i], memo[i / 2] + 1);
             }
 
-            if(n % 2 == 0 ){
-                n = n / 2;
-                continue;
+            if(i % 3 == 0) {
+                memo[i] = Math.min(memo[i], memo[i / 3] + 1);
             }
 
-            n--;
+//            System.out.println(Arrays.toString(memo));
         }
 
-        System.out.println(answer);
+
+
+        System.out.println(memo[n]);
     }
 
 }

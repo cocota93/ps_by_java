b3737ca02ac5fab3d7eb3249cb6601931e5255fd
jedy <cocota93@gmail.com>
2021-02-10 14:54:36 +0900

    부분수열의합2 : 완료. 어려웠음.

--- src/com/ps/백준/완전탐색/부분수열의합2/Main.java
+++ src/com/ps/백준/완전탐색/부분수열의합2/Main.java
@@ -1,12 +1,11 @@
 package com.ps.백준.완전탐색.부분수열의합2;
 
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 import java.io.FileInputStream;
-import java.util.StringTokenizer;
 
 /*
 * 부분수열의 합이 s가 되는 경우의 수를 구하는 문제.
@@ -17,10 +16,32 @@ import java.util.StringTokenizer;
 *
 * */
 
+/*
+* 부분수열에대해서 잘못 이해하고 있었음.
+* 부분수열이란 원래 수열의 일부 항을 순서대로 나열한것.
+*  일부 항들이 꼭 연속적일 필요는 없으며 순서만 지킨다면 부분수열을 만족.
+*
+* 기존처럼 투포인터로 풀었을때 문제는 주어진 시간안에 해결이 안된다는점.
+* 때문에 이를 해결하기 위해서는 반을 나눠서 처리해야한다고 한다. (????)
+*
+* */
+
+
+/*
+* 각 배열에 대한 카운팅이 int범위로 충분할거라 생각해서 int로 카운팅해놓고
+* 결과는 long으로 받았다.
+* long = int * int;
+* 이렇게 계산하면 곱하기의 연산 결과가 int범위를 넘어갈때 문제가된다. 떄문에
+* long = long * long으로 해야 의도에 맞게 결과가 나온다.
+* */
+
+
 class Main {
 
+    static int inputSequenceArr[];
+
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("src/com/ps/백준/완전탐색/부분수열의합2/input.txt"));
+//        System.setIn(new FileInputStream("src/com/ps/백준/완전탐색/부분수열의합2/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
@@ -29,50 +50,85 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
 
-        int arr[] = new int[n];
+        inputSequenceArr = new int[n];
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < inputSequenceArr.length; i++) {
+            inputSequenceArr[i] = Integer.parseInt(st.nextToken());
         }
 
-//        int greateMin = Math.abs(Arrays.stream(arr).min().getAsInt());
-//        s = s + greateMin + 1;
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] += greateMin + 1;
-//        }
-
-//        Arrays.sort(arr, Comparator.reverseOrder);
-//        Comparator.reverseOrder
-
-
-        int sumStartIdx = 0;
-        int sumEndIdx = 0;
-        long sum = 0;
-        int answer = 0;
+        List<Integer> leftArray = new ArrayList<>();
+        {
+            int sum = 0;
+            int startIdx = 0;
+            int endIdx = n / 2;
+            makeSumArray(sum, startIdx, endIdx, leftArray);
+        }
+        leftArray.sort(Comparator.comparingInt(Integer::intValue));
 
 
-        sum += arr[sumEndIdx];
-        sumEndIdx++;
-        while(sumStartIdx <= sumEndIdx){
-            if(sum == s){
+        List<Integer> rightArray = new ArrayList<>();
+        {
+            int sum = 0;
+            int startIdx = n / 2;
+            int endIdx = n;
+            makeSumArray(sum, startIdx, endIdx, rightArray);
+        }
+        rightArray.sort(Comparator.comparingInt(Integer::intValue));
 
-                answer++;
-                sum -= arr[sumStartIdx];
-                sumStartIdx++;
-            } else if(sum < s){
-                if(sumEndIdx >= arr.length) break;
 
-                sum += arr[sumEndIdx];
-                sumEndIdx++;
-            } else if(sum > s){
+        long answer = 0;
 
-                sum -= arr[sumStartIdx];
-                sumStartIdx++;
+        int leftSearchIdx = 0;
+        int rightSearchIdx = rightArray.size() - 1;
+        while(leftSearchIdx <= leftArray.size() - 1 && rightSearchIdx >= 0){
+            long leftSearchValue = leftArray.get(leftSearchIdx);
+            long rightSearchValue = rightArray.get(rightSearchIdx);
+            long addResult = leftSearchValue + rightSearchValue;
+            if(addResult == s){
+
+                long leftEqualValueCount = 0;
+                for (int i = leftSearchIdx; i < leftArray.size(); i++) {
+                    if(leftArray.get(i) == leftSearchValue){
+                        leftSearchIdx++;
+                        leftEqualValueCount++;
+                    }else {
+                        break;
+                    }
+                }
+
+                long rightEqualValueCount = 0;
+                for (int i = rightSearchIdx; i >= 0; i--) {
+                    if(rightArray.get(i) == rightSearchValue){
+                        rightSearchIdx--;
+                        rightEqualValueCount++;
+                    }else {
+                        break;
+                    }
+                }
+
+
+                answer += leftEqualValueCount * rightEqualValueCount;
+            } else if(addResult > s){
+                rightSearchIdx--;
+            } else if(addResult < s){
+                leftSearchIdx++;
             }
         }
 
+        //leftArray의 0과 rightArray의 0이 만드는 0 + 0형태의 공집합 제거
+        if(s == 0) answer -= 1;
 
         System.out.println(answer);
+    }
+
+    private static void makeSumArray(int sum, int startIdx, int endIdx, List<Integer> saveContainer) {
+        if(startIdx == endIdx){
+            saveContainer.add(sum);
+            return;
+        }
+
+        makeSumArray(sum + inputSequenceArr[startIdx], startIdx + 1, endIdx, saveContainer);
+        makeSumArray(sum, startIdx + 1, endIdx, saveContainer);
 
     }
 

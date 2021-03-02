d917f3e265d7173e7280786638b813e2133b59ac
jedy <cocota93@gmail.com>
2021-03-02 14:36:15 +0900

    줄세우기 : 완료. 간선에 대한 정보를 인접행렬에서 인접리스트로 바꾸어서 해결

--- src/com/ps/백준/그래프/줄세우기/Main.java
+++ src/com/ps/백준/그래프/줄세우기/Main.java
@@ -1,12 +1,7 @@
 package com.ps.백준.그래프.줄세우기;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 
 class Main {
@@ -23,20 +18,26 @@ class Main {
 
 
         int indegreeCnt[] = new int[n + 1];
+//        List<List<Integer>> edgeGraphList = new ArrayList<>();
+        List<Integer> edgeGraphList[] = new List[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            edgeGraphList[i] = new ArrayList<>();
+        }
+//        boolean edgeGraph[][] = new boolean[n + 1][n + 1];
 
-        boolean edgeGraph[][] = new boolean[n + 1][n + 1];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
 
-            edgeGraph[from][to] = true;
+//            edgeGraph[from][to] = true;
+            edgeGraphList[from].add(to);
             indegreeCnt[to]++;
         }
 
         Queue<Integer> indegreeZeroQueue = new LinkedList<>();
-        LinkedList<Integer> visitSequenceList = new LinkedList<>();
+        List<Integer> visitSequenceList = new ArrayList<>();
 
         for (int i = 1; i < indegreeCnt.length; i++) {
             if(indegreeCnt[i] == 0){
@@ -49,16 +50,22 @@ class Main {
             Integer visitPos = indegreeZeroQueue.poll();
             visitSequenceList.add(visitPos);
 
-            for (int connectedPos = 1; connectedPos < edgeGraph[visitPos].length; connectedPos++) {
-                boolean connected = edgeGraph[visitPos][connectedPos];
-                if(!connected) continue;
-
+            for (Integer connectedPos : edgeGraphList[visitPos]) {
                 indegreeCnt[connectedPos]--;
-
                 if(indegreeCnt[connectedPos] == 0){
                     indegreeZeroQueue.add(connectedPos);
                 }
             }
+//            for (int connectedPos = 1; connectedPos < edgeGraphList[visitPos].size(); connectedPos++) {
+//                boolean connected = edgeGraphList[visitPos][connectedPos];
+//                if(!connected) continue;
+//
+//                indegreeCnt[connectedPos]--;
+//
+//                if(indegreeCnt[connectedPos] == 0){
+//                    indegreeZeroQueue.add(connectedPos);
+//                }
+//            }
         }
 
         for (Integer integer : visitSequenceList) {

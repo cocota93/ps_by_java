fdb4d70b75338375f2507c1d9b0d761d0b993297
jedy <cocota93@gmail.com>
2021-03-12 12:06:12 +0900

    최소비용구하기2 : 완료. 다익스트라

--- src/com/ps/백준/그래프/최소비용구하기2/Main.java
+++ src/com/ps/백준/그래프/최소비용구하기2/Main.java
@@ -1,6 +1,7 @@
 package com.ps.백준.그래프.최소비용구하기2;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
@@ -46,6 +47,9 @@ class Main {
         visitCheck = new boolean[cityCount + 1];
 
         shortestPath = new Vertex[cityCount + 1];
+        for (int i = 0; i < shortestPath.length; i++) {
+            shortestPath[i] = new Vertex(0, Integer.MAX_VALUE);
+        }
 
         for (int i = 0; i < busCount; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -71,18 +75,19 @@ class Main {
 
             for (Vertex connectedSomething : board[visitPlace.end]) {
 
-                if(!visitCheck[connectedSomething.end])
-                    shortestPath[connectedSomething.end] = new Vertex(visitPlace.end, connectedSomething.cost + visitPlace.cost);
-                else if(shortestPath[connectedSomething.end].cost > connectedSomething.cost + visitPlace.cost)
-                    shortestPath[connectedSomething.end] = new Vertex(visitPlace.end, connectedSomething.cost + visitPlace.cost);
-                //shortestPath사용시 shortestPath[visitPlace.end]로 안하고 shortestPath[visitPlace.end] 로 구현한 이유는
-                //더 빠른길을 알게 되었을때 shortestPath[visitPlace.end] 는 한번만 변경하면 된다.
+                if(shortestPath[connectedSomething.end].cost > visitPlace.cost + connectedSomething.cost){
+                    shortestPath[connectedSomething.end] = new Vertex(visitPlace.end, visitPlace.cost + connectedSomething.cost);
+                    nextPlaceQueue.add(new Vertex(connectedSomething.end, visitPlace.cost + connectedSomething.cost));
+                }
 
-                nextPlaceQueue.add(new Vertex(connectedSomething.end, connectedSomething.cost));
-                visitCheck[connectedSomething.end] = true;
 
                 //목적지까지 가는데 비용이 작은 길이 여러번 모여서 결국 누적된 비용이 큰걸로 한번에 가는길보다 커질수 있다.
                 //때문에 D(s, e) = D(s, c) + W(c, e) 라는 수식을 적용해야 할것 같다.
+                // 큐에 넣을때 cost는 해당 지점까지의 누적비용을 넣어야됨.
+                //그래야 더 적은비용으로 갈수 있는 경로를 발견했을때 단편적으로 빠른길인지 정말로 빠른길인지 알수 있게됨.
+                //가장 작은 비용을 먼저 계산하는지, 큰걸먼저 하든지 어차피 답은 나온다. 모든 간선에 대해서 계산해보게 되기 떄문.
+                //어쩃든 그렇게해서 계산해서 도착지점에 도착했을때 누적된 코스트 비용이 가장 작은 간선을 따라가다 보면 시작지점
+                //알수있게됨.
 
             }
         }
@@ -91,6 +96,7 @@ class Main {
         {
             Vertex cur = shortestPath[endCity];
             List<Integer> pathHistory = new ArrayList<>();
+            pathHistory.add(endCity);
             while(cur.end != -1){
                 pathHistory.add(cur.end);
                 cur = shortestPath[cur.end];

d9d60c1f31c8cbcf1d7304253bd2bab2c59c1300
jedy <cocota93@gmail.com>
2021-03-15 19:00:14 +0900

    특정한 최단경로 : 완료. 다익스트라 + 특정 지점 경유

--- src/com/ps/백준/그래프/특정한최단경로/Main.java
+++ src/com/ps/백준/그래프/특정한최단경로/Main.java
@@ -28,7 +28,7 @@ class Main {
     static int INF = 987654321;
 
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("src/com/ps/백준/그래프/특정한최단경로/input.txt"));
+//        System.setIn(new FileInputStream("src/com/ps/백준/그래프/특정한최단경로/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
@@ -44,9 +44,7 @@ class Main {
         }
 
         optimalDist = new int[n + 1];
-        for (int i = 0; i < optimalDist.length; i++) {
-            optimalDist[i] = INF;
-        }
+        Arrays.fill(optimalDist, INF);
 
         //양방향그래프
         for (int i = 0; i < e; i++) {
@@ -67,21 +65,84 @@ class Main {
         //1번 정점에서 가장 마지막 정점인 n번 정점으로의 최단경로를 구하는 문제.
         //단 v1, v2를 반드시 거쳐야함.
 
-        Queue<Vertex> pq = new PriorityQueue<>(Comparator.comparingInt(vertex -> vertex.cost));
-        optimalDist[1] = 0;
-        pq.add(new Vertex(1, 0));
 
-        while(!pq.isEmpty()){
-            Vertex visitPlace = pq.poll();
+        int answer = INF;
 
-            for (Vertex connectedPlace : pathCost[visitPlace.end]) {
-                optimalDist[connectedPlace.end] = optimalDist[visitPlace.end] + connectedPlace.cost;
+        {
+            int start = 1;
+            int goal = n;
+            int cost = 0;
+
+            {
+                int calResult = Dikstra(start, v1);
+                if(calResult != INF){
+                    cost += calResult;
+                    calResult = Dikstra(v1, v2);
+
+                    if(calResult != INF){
+                        cost += calResult;
+                        calResult = Dikstra(v2, goal);
+
+                        if(calResult != INF){
+                            cost += calResult;
+
+                            answer = cost;
+                        }
+                    }
+                }
             }
         }
 
+        {
+            int start = 1;
+            int goal = n;
+            int cost = 0;
+
+            {
+                int calResult = Dikstra(start, v2);
+                if(calResult != INF){
+                    cost += calResult;
+                    calResult = Dikstra(v2, v1);
+
+                    if(calResult != INF){
+                        cost += calResult;
+                        calResult = Dikstra(v1, goal);
+
+                        if(calResult != INF){
+                            cost += calResult;
+
+                            answer = Math.min(cost, answer);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(answer == INF) System.out.println(-1);
+        else System.out.println(answer);
+    }
+
+    private static int Dikstra(int start, int end) {
+        Arrays.fill(optimalDist, INF);
+        boolean visitCheck[] = new boolean[optimalDist.length];
+
+        Queue<Vertex> pq = new PriorityQueue<>(Comparator.comparingInt(vertex -> vertex.cost));
+        optimalDist[start] = 0;
+        pq.add(new Vertex(start, 0));
+
+        while(!pq.isEmpty()){
+            Vertex visitPlace = pq.poll();
+            if(visitCheck[visitPlace.end]) continue;
+            visitCheck[visitPlace.end] = true;
 
 
+            for (Vertex connectedPlace : pathCost[visitPlace.end]) {
+                optimalDist[connectedPlace.end] = Math.min(optimalDist[visitPlace.end] + connectedPlace.cost, optimalDist[connectedPlace.end]);
+                pq.add(new Vertex(connectedPlace.end, optimalDist[connectedPlace.end]));
+            }
+        }
 
+        return optimalDist[end];
     }
 
 }

44abefb2d815d99cb8bb890e9cfb8f479bbef464
jedy <cocota93@gmail.com>
2021-04-16 16:26:30 +0900

    다리놓기 : 오답. 콤비네이션(mCn)으로 풀수있을것 같아서 그렇게 해봤는데 범위를 벗어남.

---
+++ src/com/ps/백준/DP/다리놓기/Main.java
@@ -0,0 +1,51 @@
+package com.ps.백준.DP.다리놓기;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/com/ps/백준/DP/다리놓기/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+//        long dp[] = new long[30 + 1];
+//        dp[0] = 0;
+//        dp[1] = 1;
+//        for (int i = 2; i < dp.length; i++) {
+//            dp[i] = dp[i - 1] * i;
+//        }
+
+        int t = Integer.parseInt(br.readLine());
+        for (int testCase = 0; testCase < t; testCase++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            //mcn
+
+            long topResult = 1;
+            for (long top = m; top > m - n; top--) {
+                topResult *= top;
+            }
+
+            long bottomResult = 1;
+            for (long bottom = n; bottom > 1; bottom--) {
+                bottomResult *= bottom;
+            }
+
+            long answer = topResult / bottomResult;
+
+            System.out.println(answer);
+        }
+
+
+    }
+
+}

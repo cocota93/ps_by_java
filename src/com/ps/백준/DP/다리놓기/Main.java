c551caa8bccb9c8b3c79228fdd2b4ee11f47bd9e
jedy <cocota93@gmail.com>
2021-04-22 15:37:11 +0900

    다리놓기 : 완료

--- src/com/ps/백준/DP/다리놓기/Main.java
+++ src/com/ps/백준/DP/다리놓기/Main.java
@@ -6,20 +6,21 @@ import java.util.Scanner;
 import java.io.FileInputStream;
 import java.util.StringTokenizer;
 
+//참고
+//![](Main.png)
+
 
 class Main {
 
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("src/com/ps/백준/DP/다리놓기/input.txt"));
+//        System.setIn(new FileInputStream("src/com/ps/백준/DP/다리놓기/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        long dp[] = new long[30 + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = dp[i - 1] * i;
+        long dp[][] = new long[30 + 1][30 + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][1] = i;
         }
 
         int t = Integer.parseInt(br.readLine());
@@ -30,15 +31,14 @@ class Main {
 
             //mcn
 
-            long topResult = dp[m] / dp[m - n];
+            for (int i = 2; i <= m; i++) {
+                for (int j = 2; j <= i; j++) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+            }
 
-            long bottomResult = dp[n];
-
-            long answer = topResult / bottomResult;
-
-            System.out.println(answer);
+            System.out.println(dp[m][n]);
         }
-
 
     }
 

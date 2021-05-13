75525b00a14aa26dacc03bd953db3f450d513c3d
jedy <cocota93@gmail.com>
2021-05-13 10:35:23 +0900

    피보나치수7 : 완료

---
+++ src/com/ps/백준/DP/피보나치수7/Main.java
@@ -0,0 +1,31 @@
+package com.ps.백준.DP.피보나치수7;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.StringTokenizer;
+
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+//        System.setIn(new FileInputStream("src/com/ps/백준/DP/피보나치수7/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+//        st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[1000001];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] % 1000000007) ;
+        }
+
+        System.out.println(dp[n] % 1000000007);
+    }
+
+}
